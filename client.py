e8b521c88bf8c97ece92068e560eb1c35daf1945
Alex Guevara <abg28@duke.edu>
2018-11-07 21:52:15 -0500

    Adds gunicorn to required packages.  Adds client.py.

---
+++ client.py
@@ -0,0 +1,15 @@
+import requests
+
+# Tests POST /distance endpoint
+r = requests.post("vcm-7291.vm.duke.edu:5000/distance",
+                  json={"a": [2, 4],
+                        "b": [5, 6]})
+print(r.json())
+
+# Tests GET /name endpoint
+r = requests.get("vcm-7291.vm.duke.edu:5000/name")
+print(r.json())
+
+# Tests GET /hello/<name> endpoint
+r = requests.get("vcm-7291.vm.duke.edu:5000/hello/Alex")
+print(r.json())
